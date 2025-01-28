89c1341530396a040829136a50b5ec54fa98d6d1
Jakkraphop Pengchan <jakkph32@gmail.com>
2025-01-28 09:15:27 +0700

    Update version to 0.0.23
    Update ESLint config and package version
    
    - Change ESLint sourceType to commonjs
    - Update package.json version to 0.0.23
    - Update CLI script to use require instead of import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   {
--- scripts/cli.js
+++ scripts/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { join } from "path";
-import starter from "./starter.js";
+const { join } = require("path");
+const starter =require("./starter.js")
 const destination = getDest(process.argv[2]);
 
 function getDest(destFolder = "mts-app") {
